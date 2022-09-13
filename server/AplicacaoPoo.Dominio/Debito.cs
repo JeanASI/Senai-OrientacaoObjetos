477dc9ecd476620d65cb82a83c72c5afb73617d7
JeanASI <jeanderson.a.silva@aluno.senai.br>
2022-09-12 21:56:30 -0300

    #1 Inicializando

---
+++ server/AplicacaoPoo.Dominio/Debito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoPoo.Dominio
+{
+    internal class Debito
+    {
+        Pagamento   Pagamento { get; set; }
+        double  Desconto { get; set; }
+
+    }
+}
